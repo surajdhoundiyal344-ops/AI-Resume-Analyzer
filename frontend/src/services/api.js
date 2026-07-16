609bd09e99af95adc6e739d06d1566c7b87c346f
suraj <surajdhoundiyal344@gmail.com>
2026-07-16 19:54:07 +0530

    Update backend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ai-resume-analyzer-f3ja.onrender.com",
+  baseURL: "https://ai-resume-analyzer-xxc9.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
